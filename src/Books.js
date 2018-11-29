c3b314cd4bacc6024a89f7c50d2759fd89439323
iliav <iliav@hurdit.com>
2018-11-29 10:41:27 -0500

    GraphQL integration

---
+++ src/Books.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import Book from './Book';
+
+const Books = () => (
+  <Query
+    query={gql`
+      {
+        allBooks {
+          id
+          title
+          author
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Good things take time....</p>;
+      if (error) return <p>Something went wrong...</p>;
+
+      return (
+        <div className="row">
+          {data.allBooks.map(book => (
+            <Book book={book} />
+          ))}
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default Books;
